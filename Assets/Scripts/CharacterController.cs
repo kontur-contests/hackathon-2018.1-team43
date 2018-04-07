cefd98da9435f54e9e7ccab1dd538180ebe86ac5
Ivan Modin <raijusiju@gmail.com>
2018-04-07 15:50:46 +0400

    Add CharacterController

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour {
+    public float RotationSensivity;
+    public float MovementSpeed;
+
+    private Animator animator;
+    private Rigidbody rigidbody;
+    private float moveSpeed;
+
+	// Use this for initialization
+	void Start ()
+    {
+        animator = GetComponent<Animator>();
+        rigidbody = GetComponent<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        var horizontal = Input.GetAxis("Horizontal");
+        var vertical = Input.GetAxis("Vertical");
+
+        var axisDirection = new Vector3(horizontal, 0, vertical);
+
+        if (axisDirection.magnitude > 0)
+        {
+            var newRotation = Quaternion.LookRotation(axisDirection.normalized);
+            rigidbody.rotation = Quaternion.LerpUnclamped(rigidbody.rotation, newRotation, RotationSensivity * Time.deltaTime);
+        }
+
+        var moveSpeed = 0.0f;
+        var animatorState = animator.GetCurrentAnimatorStateInfo(0);
+        if(animatorState.IsName("Walking"))
+        {
+            moveSpeed = MovementSpeed * 0.3f;
+        }
+        else if (animatorState.IsName("Running"))
+        {
+            moveSpeed = MovementSpeed * 0.6f;
+        }
+        else if (animatorState.IsName("Sprint"))
+        {
+            moveSpeed = MovementSpeed;
+        }
+
+        rigidbody.MovePosition(transform.position + axisDirection.normalized * moveSpeed * Time.deltaTime);
+
+        animator.SetFloat("Movement Speed", axisDirection.magnitude * 11);
+    }
+}
